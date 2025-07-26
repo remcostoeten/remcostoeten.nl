f0ba8488076a57cd54a9adf2ed89b4293c913733
Remco Stoeten <remcostoeten@hotmail.com>
2025-07-26 14:32:22 +0200

    Fix TanStack Solid Query setup
    - Properly configure QueryClientProvider in app root
    - Set up QueryClient with sensible defaults
    - Resolves QueryClientProvider mounting issues

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,28 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import "./app.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export default function App() {
   return (
-    <Router
-      root={props => (
-        <>
-          <Nav />
+    <QueryClientProvider client={queryClient}>
+      <Router
+        root={props => (
           <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+    </QueryClientProvider>
   );
 }
