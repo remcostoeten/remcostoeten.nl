6f2096dbc54ef033fdfc245950a2aa9052f619b1
Remco Stoeten <remcostoeten@hotmail.com>
2025-11-28 23:34:02 +0100

    feat: add shared UI primitives and announcement system
    Create reusable UI primitives including announcement banner, theme provider, and contact button components.

---
+++ src/components/announcement-banner.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import * as React from "react"
+import { ArrowUpRightIcon } from "lucide-react"
+import {
+  AnnouncementBanner,
+  Announcement,
+  AnnouncementTag,
+  AnnouncementTitle,
+  AnnouncementClose
+} from "@/shared/primitives"
+import { usePersistence } from "@/shared/primitives"
+
+export function SiteAnnouncementBanner() {
+  const announcement = usePersistence({
+    id: "site-blog-announcement",
+    storageType: "localStorage",
+    autoShow: true,
+    showDelay: 2000, // Show after 2 seconds
+    maxDismissals: 5,
+  })
+
+  const handleClose = () => {
+    announcement.dismiss()
+  }
+
+  return (
+    <AnnouncementBanner
+      isVisible={announcement.isVisible}
+      position="top-center"
+      draggable={true}
+      hideOnScroll={true}
+      scrollThreshold={100}
+      hideAfterPixels={300}
+      showAgain="top"
+      onClose={handleClose}
+    >
+      <Announcement>
+        <AnnouncementTag>Blog</AnnouncementTag>
+        <AnnouncementTitle showIcon icon={<ArrowUpRightIcon size={16} />}>
+          <span>Read about the over-engineering of my new site!</span>
+        </AnnouncementTitle>
+      </Announcement>
+      <AnnouncementClose onClose={handleClose} />
+    </AnnouncementBanner>
+  )
+}
