de492ae3f05f181371bbd8cdf075ecf6fa91af83
Remco Stoeten <remcostoeten@hotmail.com>
2026-01-16 01:28:59 +0100

    fix(build): remove extra closing div in contact-overview

--- src/components/admin/contact/contact-overview.tsx
+++ src/components/admin/contact/contact-overview.tsx
@@ -150,7 +150,7 @@ export function ContactOverview({ data }: { data: ContactStats }) {
                         )}
                         </div>
 
-                </div>
+
             </CardContent>
         </Card>
     )
